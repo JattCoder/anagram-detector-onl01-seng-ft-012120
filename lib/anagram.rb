5dee62ef68f6b6a7e6177a35af1ede7ee69ebb9f
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-02 01:22:44 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word, :arr
+  def initialize(word)
+    @word = word
+    @arr = []
+  end
+
+  def match(sentence)
+    sentence.each do |word|
+      @arr << word if word.chars.sort.join == @word.chars.sort.join
+    end
+    return @arr
+  end
+end
