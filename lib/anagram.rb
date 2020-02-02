c73ff00a065d482b4a900abe5aacb966fd278483
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-02 01:13:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,14 @@
 class Anagram
-
+  attr_accessor :word :arr
   def initialize(word)
     @sword = word
     @wordarr = []
   end
 
   def match(sentence)
-    sentence = %w(sentence).split("\n")
     sentence.each do |word|
       @wordarr << word if word.char.sort.join == @sword
     end
     return @wordarr
   end
-
 end
