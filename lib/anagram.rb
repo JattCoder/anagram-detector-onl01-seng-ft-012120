d005a6241799292e26964012618e46c38ed47015
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-02 00:58:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @sword = word
+    @wordarr = []
+  end
+
+  def match(sentence)
+    sentence = %w(sentence).split("\n")
+    sentence.each do |word|
+      @wordarr << word if word.char.sort.join == @sword
+    end
+    return @wordarr
+  end
+
+end
